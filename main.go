bb631bab95e5cb007aeb641e2602a352bba58a65
opkpEh <harshpandeykp20@gmail.com>
2025-02-22 02:28:03 +0530

    base

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
@@ -19,15 +20,20 @@ var (
 )
 
 func mongoInit() {
-	if uri = os.Getenv("MONGODB_URI"); uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	err := godotenv.Load("myenv.env")
+	if err != nil {
+		log.Fatal("Error loading env")
 	}
+
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable in myenv.env file")
+	}
+
 	ctx = context.Background()
 
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 
-	var err error
 	client, err = mongo.Connect(opts)
 	if err != nil {
 		panic(err)
